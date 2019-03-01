d89bed0106053d75f29349622ead393199c4900d
Alicia Santiago <albrown07@mac.com>
2019-03-01 16:47:54 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative 'fancy_dance.rb'
 
-class Dancer
+class Dancer::FancyDance
   # extend FancyDance::ClassMethods
   # include FancyDance::InstanceMethods
   extend MetaDancing
